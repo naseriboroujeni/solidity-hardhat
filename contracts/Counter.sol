4e7e3873bdf328342c50b5f0063ffbed3f648c84
naseriboroujeni <naseriboroujeni@gmail.com>
2024-09-07 20:35:08 +0300

    Add counter contract

---
+++ contracts/Counter.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+contract Counter {
+    uint8 private count;
+    string public name;
+
+    constructor(uint8 _initialCount, string memory _name) {
+        count = _initialCount;
+        name = _name;
+    }
+
+    function increaseCount() public returns (uint8) {
+        return ++count;
+    }
+
+    function decreaseCount() public returns (uint8) {
+        return count--;
+    }
+
+    function getCount() public view returns (uint8) {
+        return count;
+    }
+
+    function setName(string calldata _newName) public returns (string memory) {
+        name = _newName;
+        return name;
+    }
+
+    function sumOfArray(uint8[] calldata array) public pure returns (uint8) {
+        uint8 sum = 0;
+        for (uint8 i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return sum;
+    }
+}
